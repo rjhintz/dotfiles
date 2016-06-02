7066e1b068e40c9b5facdfbe968f72789a2e8df2
Rich Hintz <rjhintz@gmail.com>
2016-06-02 12:58:09 -0700

    initial - setup.sh

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+#
+# Setup links
+#
+sudo ln -s ~/dotfiles/gitconfig ~/.gitconfig
+#
+sudo ln -s ~/dotfiles/profile ~/.profile  #  <--may not work for AWS
+#
+sudo ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
+#
+sudo ln -s ~/dotfiles/vimrc ~/.vimrc
+#
