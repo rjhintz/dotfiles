4a08ff6ab5338dc486513cf9ba4a388cc616f5cb
Rich Hintz <rjhintz@gmail.com>
2016-06-02 13:34:09 -0700

    clone Vundle

--- setup.sh
+++ setup.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 #
+# Get code
+#
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 # Setup links
 #
 sudo ln -s ~/dotfiles/gitconfig ~/.gitconfig
@@ -9,4 +12,4 @@ sudo ln -s ~/dotfiles/profile ~/.profile  #  <--may not work for AWS
 sudo ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
 #
 sudo ln -s ~/dotfiles/vimrc ~/.vimrc
-#
+# End
